3cb0a8ee503df02da512c6d9349bdd2380cdb57c
Jsala <juliosalag@gmail.com>
2023-10-18 21:34:07 +0200

    Colisiones con el techo a mitad

--- game/src/sys/collision.s
+++ game/src/sys/collision.s
@@ -4,19 +4,62 @@
 .include "man/entity.h.s"
 .include "cpctelera.h.s"
 
+
+is_colliding_player:: ;; 0 no collision | 1 collision
+    .db 0x00       ;; up
+    .db 0x00       ;; down
+    .db 0x00       ;; right
+    .db 0x00       ;; left
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; INITIALIZE is_colliding_player
+;;
+sys_initialize_collision_player_tilemap:
+    ld      a, #0
+    ld      (is_colliding_player), a 
+    ld      (is_colliding_player + 1), a 
+    ld      (is_colliding_player + 2), a 
+    ld      (is_colliding_player + 3), a 
+
+    ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Check collisions player with tilemap
+;;
+sys_collision_player_tilemap:
+
+    ld      a, #1
+    ld      (is_colliding_player), a 
+
+    ret
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; UPDATE ONE ENTITY WITH THE TILEMAP 
-;;  Input:
-;;     IX: Pointer to entity having movement and collider component
 ;; 
 sys_collision_update_one_entity:
+
+    ld hl, #TYPE
+    add hl, de
+    ld a, (hl)
+    cp #E_TYPE_PLAYER
+    jr z, iniciar_colision
+    jr comprobar_colision
+    iniciar_colision:
+    call sys_initialize_collision_player_tilemap
+
+    comprobar_colision:
+
     ;; tx = x/4
     ;; ty = y/8
     ;; tw = tilemap-width (0x30, 48)
     ;; p  = tilemap + ty * tw + tx
 
+    push de
+
     ;; A = y
-    ld    a, Y(ix)
+    ld   hl, #Y
+    add  hl, de
+    ld    a, (hl)
     ;; Para calcular el sprite de abajo sumar el height (add #15)
     ;; A = ty (y/8)
     and   #0xF8 ;; #0xb11111000 ;; A = 8*int(ty / 8)
@@ -30,7 +73,9 @@ sys_collision_update_one_entity:
     add   hl, hl   ;; HL = 32*tx
     add   hl, de   ;; HL = 48*tx
     ;; A = X
-    ld     a, X(ix)
+    ld   hl, #X
+    add  hl, de
+    ld    a, (hl)
     srl    a ;; | A = tx (x/4)
     srl    a ;; |
 
@@ -38,15 +83,33 @@ sys_collision_update_one_entity:
     ld     de, #_tilemap_01
     add    hl, de
 
+    pop de
+
     ;; HL = tilemap + ty * tw + tx
     ld      a, (hl)
     and     #0b11111110
     ret     nz
 
-    ld      a, #0x33 ; 4F07
+    ld hl, #TYPE
+    add hl, de
+    ld a, (hl)
+    cp #E_TYPE_PLAYER
+    jr z, finalizar_colision
+    jr final_colision
+    finalizar_colision:
+    call sys_collision_player_tilemap
 
-    ;; velocidad a 0
+    final_colision:
+
     ;; reposicionar la entidad
+    ;;ld   hl, #Y
+    ;;add  hl, de
+    ;;ld    a, (hl)
+    ;;inc   a
+    ;;ld  (hl), a
+    ;;
+    ;;;; velocidad a 0
+    ;;call stop_sprite
 
     ret
 _sys_collision_update::
